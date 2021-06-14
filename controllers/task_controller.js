b1a2e79a154278900c4b9a7144d1f345f9ce4242
wirery <wire_ryan@yahoo.com>
2021-06-14 11:34:02 -0400

    Index route created

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -13,4 +13,22 @@ tasks.post('/', async (req, res) => {
     res.status(200).send(createdTask) //  .json() will send proper headers in response so client knows it's json coming back
   })
 })
+
+//curl -X POST -H "Content-Type: application/json" -d '{"name":"world kindness"}' http://localhost:3003/tasks
+
+
+
+//Index route
+tasks.get('/', (req, res) => {
+    Task.find({}, (err, foundTasks) => {
+        if (err) {
+          res.status(400).json({ error: err.message })
+        }
+        res.status(200).json(foundTasks)
+      })
+    })
+    //curl http://localhost:3003/tasks
+
+  
+      
 module.exports = tasks
