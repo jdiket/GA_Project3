ce690a92753c3efeb434901240f0ce2e1445fb5c
wirery <wire_ryan@yahoo.com>
2021-06-14 13:59:33 -0400

    Server updated with mongoose and cors connections

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const mongoose = requre ('mongoose')
+
 
 // CONFIG
 const app = express();
 const PORT = 3003;
+
+
+const cors = require('cors')
+
+
+
+//Middleware
+
+app.use(express.json()) // use .json(), not .urlencoded()
+
+const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions)) 
+
+//Database Error/Disconnection
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+//Database Connection
+mongoose.connect('mongodb://localhost:27017/tasks', { useNewUrlParser: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+})
+
+//Controllers/Routes
+const tasksController = requre('./controllers/tasks.js')
+app.use('/tasks', tasksController)
 
 // LISTENER
 app.listen(PORT, () => {console.log('five by five on ', PORT)});
