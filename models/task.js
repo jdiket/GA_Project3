e3fe03efd678de6dd6ec2326f5057375e24204a8
Troy Bankerd <troybankerd@troys-mbp.lan>
2021-06-14 11:32:48 -0400

    Started model

---
+++ models/task.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    creator: {type: String, required: true},
+    dateDue: {type: String, required: true},
+    description: {type: String, required: true},
+    
+
+})
+
+
+module.exports = mongoose.model('Task', taskSchema);
