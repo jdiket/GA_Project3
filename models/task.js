f32286331dfcf429936df6982cb838a0235f7bb5
Troy Bankerd <troybankerd@troys-mbp.lan>
2021-06-14 13:07:33 -0400

    Updated task schema

--- models/task.js
+++ models/task.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema({
     name: {type: String, title: 'Name', required: true},
-    creator: {type: String, title: 'Creator', required: true},
-    completed:{type: Boolean, title: 'Completed', required: true},
+    creator: {type: String, title: 'Creator'},
+    completed:{type: Boolean, title: 'Completed'},
     dateDue: {type: String, title: 'Date Due', required: true},
-    description: {type: String, title: 'Description', required: true}, subTask: [{
-        description: {type: String, title: 'Description', required: true},
-        completed:{type: Boolean, title: 'Completed', required: true}
+    description: {type: String, title: 'Description'}, subTask: [{
+        description: {type: String, title: 'Description'},
+        completed:{type: Boolean, title: 'Completed'}
     }]
     
 
